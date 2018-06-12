d7425a3965beca40a36d7b532e895f1bfe84eeb4
Eric Shull and Reese Glasscock <reese@>
2018-06-12 16:25:08 -0700

    add scripts to output movie info

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// constructor for ticket
 var ticket = function Ticket(movie, time, age, price) {
   this.movie = movie;
   this.time = time;
@@ -5,37 +6,85 @@ var ticket = function Ticket(movie, time, age, price) {
   this.price = [];
 }
 
+// method for price
 ticket.prototype.pricing = function (price) {
-  var price = 5
-  if (this.age >= 65) {
+  if (this.age == "true" || this.time < 17) {
+    console.log(this.age);
+    console.log("discount");
     return this.price = 5;
-    console.log("senior discount");
   }
-  else if (this.age < 65) {
+  else if (this.age == "false" || this.time >= 17){
     return this.price = 10;
   }
 }
 
-ticket.prototype.yourTicket = function (yourTicket) {
-  return this.movie +" "+ this.time +" "+ this.age +" "+ this.price
+ticket.prototype.timeChange = function (hour) {
+  console.log(hour);
+  if (this.time < 12) {
+    return this.time + ":00am "
+  } else if (this.time >= 12) {
+    return (this.time - 12) + ":00pm "
+
+  }
+}
+
+ticket.prototype.yourMovie = function (yourMovie) {
+  return this.movie
 };
 
+function timeSelector(movieTitle) {
+  var wonderwoman = $("input:radio[name=wonderwoman]:checked").val();
+  var batman = $("input:radio[name=batman]:checked").val();
+  var superman = $("input:radio[name=superman]:checked").val();
+
+    if (batman > 0) {
+      return batman;
+    } else if (wonderwoman > 0) {
+      return wonderwoman
+    } else if (superman > 0) {
+      return superman;
+    }
+}
+
+function movieSelector(movieTitle) {
+  var wonderwoman = $("input:radio[name=wonderwoman]:checked").val();
+  var batman = $("input:radio[name=batman]:checked").val();
+  var superman = $("input:radio[name=superman]:checked").val();
+
+    if (batman > 0) {
+      console.log("wonderwoman reached");
+      return "Batman Returns";
+    } else if (wonderwoman > 0) {
+      console.log("wonderwoman reached");
+      return "Wonder Woman";
+    } else if (superman > 0) {
+      return "Superman";
+    }
+}
+
+// method to concatenate the information into a div
 
 // UI Logic
 
 $(function() {
   $("#ticketForm").submit(function(event) {
     event.preventDefault();
-    console.log("It works");
-    var movie = $("#movie").val();
-    var time = $("#time").val();
-    var age = $("#age").val();
-
+    var movie = movieSelector();
+    var time = timeSelector();
+    // var age = $("#age").val();
+    // var age = $("#age").val();
+    var age = $("input:radio[name=age]:checked").val();
     var individualTicket = new ticket(movie, time, age)
 
-    $("#output").text(individualTicket.yourTicket());
-    $("#output").append(individualTicket.pricing());
+
+    $("#timeOfMovie").append(individualTicket.timeChange());
+    $("#titleOfMovie").append(individualTicket.yourMovie());
+    $("#priceOfMovie").append(individualTicket.pricing());
+
+    console.log("This is age" + age);
     console.log(individualTicket);
+    $("#ticketForm")[0].reset();
+    $("#empty").clear();
   });
 
 });
