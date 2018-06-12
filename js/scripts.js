0c5c409c9d83ddee8249e8b8c42193faab8a29a2
Eric Shull and Reese Glasscock <eric@>
2018-06-12 11:20:24 -0700

    add pricing method to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,40 @@ var ticket = function Ticket(movie, time, age, price) {
   this.movie = movie;
   this.time = time;
   this.age = age;
-  this.price = price;
+  this.price = [];
 }
 
-ticket.prototype.price = function (cost) {
-
-};
-
-ticket.prototype.seniorDiscount = function (price) {
+ticket.prototype.pricing = function (price) {
+  var price = 5
   if (this.age >= 65) {
-    return this.price = price - 2;
-
+    return this.price = 5;
+    console.log("senior discount");
   }
   else if (this.age < 65) {
-    return this.price;
+    return this.price = 10;
   }
 }
+
+ticket.prototype.yourTicket = function (yourTicket) {
+  return this.movie +" "+ this.time +" "+ this.age +" "+ this.price
+};
+
+
+// UI Logic
+
+$(function() {
+  $("#ticketForm").submit(function(event) {
+    event.preventDefault();
+    console.log("It works");
+    var movie = $("#movie").val();
+    var time = $("#time").val();
+    var age = $("#age").val();
+
+    var individualTicket = new ticket(movie, time, age)
+
+    $("#output").text(individualTicket.yourTicket());
+    $("#output").append(individualTicket.pricing());
+    console.log(individualTicket);
+  });
+
+});
